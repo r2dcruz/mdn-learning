c0a5d2eb129d1038b4bde75b9ea6bd2b2fd61dec
Romaine De La Cruz <r2dcruz@gmail.com>
2019-06-26 22:23:31 -0700

    styling tables - awesome exercise

--- box-model-recap/box-sizing-example.js
+++ box-model-recap/box-sizing-example.js
@@ -4,7 +4,7 @@ var two = document.querySelector ('.two');
 function outputWH(box) {
     var width = box.offsetWidth;
     var height = box.offsetHeight;
-    box.textContent = 'width:' + width + 'px, Height: ' + height + 'px. ';
+    box.textContent = 'width: ' + width + 'px, height: ' + height + 'px. ';
 }
 
 outputWH(one);
